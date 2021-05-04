5b7a6f587a92fbf1cc754bcead9feac2b9596141
lordrohit <83587682+lordrohit@users.noreply.github.com>
2021-05-04 11:42:35 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "1780374258:AAH85TL1nc2NRsQPRKkwn1-1f9fAM66_PaM"
+    APP_ID = "5564160"
+    API_HASH = "2b5560d15e0f9083939429add4d5b817"
+    DATABASE_URL = "postgres://ltntacbdjitlyp:8bed4074b03a81cb610ecf857ee69cba2e938fcc81e24ab98a8041598a806e7a@ec2-54-167-152-185.compute-1.amazonaws.com:5432/d5hof6m8pcl6gn"
+    SUDO_USERS = "1601731267" 
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
