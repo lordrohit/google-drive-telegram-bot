336eef586cce0f4eea49ec04076710917a906a83
lordrohit <83587682+lordrohit@users.noreply.github.com>
2021-05-04 12:41:33 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    BOT_TOKEN = "1794937970:AAGCdc3vkMmh0mdH9KiavK6ZDwDLRRI7uUY"
+    APP_ID = "5564160"
+    API_HASH = "2b5560d15e0f9083939429add4d5b817"
+    DATABASE_URL = "postgres://cladaixerdqvup:d126064ae8f279adfccffe17ef1490aa3a01547c8fd60e43a1215ef433471cad@ec2-52-87-107-83.compute-1.amazonaws.com:5432/d1cjts526j0id1"
+    SUDO_USERS = "1601731267" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/mechanical movies"
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
     G_DRIVE_CLIENT_SECRET = ""
